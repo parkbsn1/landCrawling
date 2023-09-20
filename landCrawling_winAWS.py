4373d44b3220b4acd22b037cc85134eca6587678
parkbsn1 <77330936+parkbsn1@users.noreply.github.com>
2023-09-20 15:44:34 +0900

    Add files via upload
    크롤링 함수 분리

--- landCrawling_winAWS.py
+++ landCrawling_winAWS.py
@@ -71,6 +71,19 @@ class landCrawling():
         self.logger.addHandler(file_handler)
         self.logger.addHandler(stream_handler)
 
+    def print_config_info(self):
+        self.logger.info(f"os_name: {self.os_name}")
+        self.logger.info(f"data_dir: {self.data_dir}")
+        self.logger.info(f"url_list: {self.url_list}")
+        self.logger.info(f"confirm_limit_day: {self.confirm_limit_day}")
+        self.logger.info(f"time_sleep_int1: {self.time_sleep_int1}")
+        self.logger.info(f"time_sleep_int2: {self.time_sleep_int2}")
+        self.logger.info(f"time_sleep_int3: {self.time_sleep_int3}")
+        self.logger.info(f"mail_send_flag: {self.mail_send_flag}")
+        self.logger.info(f"datetime_now: {self.datetime_now}")
+        self.logger.info(f"--------------------------------------------------")
+
+
     def start_crawling(self, urls) -> list:
         self.logger.info("start landCrawling")
         landResult = []
@@ -89,119 +102,135 @@ class landCrawling():
         driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
         for landName, url in urls.items():
-            try:
-                # URL 호출
-                driver.get(url)
-                driver.implicitly_wait(time_to_wait=self.time_sleep_int3)
-                time.sleep(self.time_sleep_int2)
-                complex_title = driver.find_element(By.ID, "complexTitle").text
-                self.logger.info("-" * 50)
-                self.logger.info(f"{complex_title} Crawling Start!")
-                crawledItems = driver.find_elements(By.CLASS_NAME,"item_inner")
-
-                #매물 건수 파싱 -> land_count = {'매매': 33, '전세': 28}
-                html = driver.page_source
-                soup = bs(html, 'html.parser')
-                tmp = soup.findAll('div', 'complex_infos')
-                land_count = {}
-                time.sleep(3)
-                for index, landTitle in enumerate(tmp):
-                    # self.logger.info(f"{landTitle.find('div', 'complex_title').text}")
-                    if landTitle.find('div', 'complex_title').text != complex_title:
-                        continue
-                    for index2, landType in enumerate(landTitle.findAll('button', 'article_link')):
-                        if landType.find('span', 'type').text in ['매매','전세','월세']:
-                            self.logger.info(f"{complex_title}: {landType.find('span', 'type').text} {int(landType.find('span', 'count').text)}")
-                            land_count[landType.find('span', 'type').text] = int(landType.find('span', 'count').text)
-                self.logger.info(f"매물 개수: {land_count}")
-
-                time.sleep(self.time_sleep_int2)
-                #매물정보가 없는 경우
-                if len(crawledItems) == 0:
-                    self.logger.info(f"{complex_title} 매물 정보 없음")
-                    continue
-
-                #매물이 소수(5개 이하)인 경우
-                elif len(crawledItems) < 5:
-                    html = driver.page_source
-                    soup = bs(html, 'html.parser')
-                    self.logger.info(f"{1}: 수집({len(crawledItems)}) | {round((len(crawledItems) / len(crawledItems)) * 100)}% )")
-
-                #매물이 다수인 경우
+            try_cnt = 0
+            while(try_cnt < 3):
+                land_info = self.get_land_info(driver, landName, url)
+                if land_info != -1:
+                    landResult.extend(land_info)
+                    break
+                elif(land_info == -2):
+                    break
                 else:
-                    time.sleep(3)
-                    maxLen = len(crawledItems)
-                    # time_value = 5
-                    totalLandCount = sum([v for v in land_count.values()])
-                    for i in range(50):
-                        if len(crawledItems) == totalLandCount:
-                            break
-                        crawledItems = driver.find_elements(By.CLASS_NAME, "item_inner")
-                        # if maxLen == len(crawledItems) and totalLandCount > maxLen:
-                        #     time.sleep(time_value)
-                        #     time_value = time_value + 1
-                        # else:
-                        #     time_value = 5
-                        click_idx = -1
-                        try:
-                            time.sleep(self.time_sleep_int2)
-                            if "네이버에서 보기" in crawledItems[click_idx].text:
-                                view_naver = crawledItems[click_idx].find_element(By.LINK_TEXT, '네이버에서 보기')
-                                time.sleep(self.time_sleep_int1)
-                                view_naver.click()
-                            else:
-                                time.sleep(self.time_sleep_int1)
-                                crawledItems[click_idx].click()
-                        except Exception as ex:
-                            self.logger.critical(f"click error")
-
-                        html = driver.page_source
-                        # soup에 넣어주기
-                        soup = bs(html, 'html.parser') #.encode("utf-8")
-                        self.logger.info(
-                            f"{i + 1}: 수집({len(crawledItems)}) | 대상({totalLandCount}) | {round((len(crawledItems) / totalLandCount) * 100)}% )")
-                        if len(crawledItems) <= maxLen and len(crawledItems) >= totalLandCount:
-                            break
-                        else:
-                            maxLen = len(crawledItems)
-                landItems = soup.findAll('div', 'item_inner')
-                self.logger.info(f"{complex_title} 매물 수집({len(crawledItems)}건) 완료")
-                res = []
-                except_cnt = 0
-                for index, value in enumerate(landItems):
-                    try:
-                        if value.find('em', 'data').string < self.confirm_limit_day:
-                            except_cnt = except_cnt + 1
-                            continue
-                        tmp_dict = {}
-                        tmp_dict["confirm_date"] = value.find('em', 'data').string  # 23.07.13.
-                        tmp_dict["complex_title"] = landName #complex_title
-                        # tmp_dict["land_title"], tmp_dict["dong"] = (value.find('div','item_title').find('span','text').string).split(' ') #샛별삼부 408동
-                        tmp_dict["item_title"] = value.find('div', 'item_title').find('span', 'text').string  # 샛별삼부 408동
-                        tmp_dict["price_type"] = value.find('div', 'price_line').find('span', 'type').string #전세
-                        if tmp_dict["price_type"] in ['매매','전세']: #매매, 전세
-                            tmp_dict["price"] = value.find('div', 'price_line').find('span', 'price').string #7억 5,000
-                            tmp_dict["price_int"] = self.get_price_int(value.find('div', 'price_line').find('span', 'price').string, tmp_dict["price_type"])
-                        else: #월세
-                            tmp_dict["price"] = value.find('div', 'price_line').get_text()[2:]
-                            tmp_dict["price_int"] = self.get_price_int(tmp_dict["price"].split('/')[0])
-                        tmp_dict["land_type"] = value.find('div', 'info_area').find('strong','type').string #아파트
-                        # tmp_dict["info_area_spec"] = (value.find('div', 'info_area').find('span','spec').string).replace('\n','') #158/128m², 15/25층, 남동향
-                        tmp_dict["area_m"], tmp_dict["floor"], tmp_dict["direction"] = (value.find('div', 'info_area').find('span', 'spec').string).replace('\n', '').split(',')  # 158/128m², 15/25층, 남동향
-                        tmp_dict["area_p"] = self.get_area_num(tmp_dict["area_m"])
-                        tmp_dict["info_area_spec2"] = (value.find('div', 'info_area').get_text().split(',')[-1]).replace('\n','').strip() #8월중입주협의
-                        #tmp_dict["raw_text"] = value.get_text().replace('\n','')
-                        res.append(tmp_dict)
-                    except Exception as ex:
-                        self.logger.critical(f"{complex_title} 파싱 실패: {value}")
-                landResult.extend(res)
-                self.logger.info(f"{complex_title}: {len(res)}건 (*{self.confirm_limit_day}이전: {except_cnt}건 )")
-            except Exception as ex:
-                self.logger.critical(f"{url} landCrawling 실패 {ex}")
-
+                    try_cnt = try_cnt + 1
+                    driver.refresh()
+                    self.logger.info(f"{landName} {try_cnt}회 실패")
         driver.quit()
         self.logger.info(f"총 {len(landResult)}건 파싱 완료")
         return landResult
+
+    def get_land_info(self, driver,landName, url):
+        try:
+            # URL 호출
+            driver.get(url)
+            driver.implicitly_wait(time_to_wait=self.time_sleep_int3)
+            time.sleep(self.time_sleep_int2)
+            complex_title = driver.find_element(By.ID, "complexTitle").text
+            self.logger.info("-" * 50)
+            self.logger.info(f"{complex_title} Crawling Start!")
+            crawledItems = driver.find_elements(By.CLASS_NAME,"item_inner")
+
+            #매물 건수 파싱 -> land_count = {'매매': 33, '전세': 28}
+            html = driver.page_source
+            soup = bs(html, 'html.parser')
+            tmp = soup.findAll('div', 'complex_infos')
+            land_count = {}
+            time.sleep(3)
+            for index, landTitle in enumerate(tmp):
+                # self.logger.info(f"{landTitle.find('div', 'complex_title').text}")
+                if landTitle.find('div', 'complex_title').text != complex_title:
+                    continue
+                for index2, landType in enumerate(landTitle.findAll('button', 'article_link')):
+                    if landType.find('span', 'type').text in ['매매','전세','월세']:
+                        self.logger.info(f"{complex_title}: {landType.find('span', 'type').text} {int(landType.find('span', 'count').text)}")
+                        land_count[landType.find('span', 'type').text] = int(landType.find('span', 'count').text)
+            self.logger.info(f"매물 개수: {land_count}")
+
+            time.sleep(self.time_sleep_int2)
+            #매물정보가 없는 경우
+            if len(crawledItems) == 0:
+                self.logger.info(f"{complex_title} 매물 정보 없음")
+                return -1
+
+            #매물이 소수(5개 이하)인 경우
+            elif len(crawledItems) < 5:
+                html = driver.page_source
+                soup = bs(html, 'html.parser')
+                self.logger.info(f"{1}: 수집({len(crawledItems)}) | {round((len(crawledItems) / len(crawledItems)) * 100)}% )")
+
+            #매물이 다수인 경우
+            else:
+                time.sleep(3)
+                maxLen = len(crawledItems)
+                # time_value = 5
+                totalLandCount = sum([v for v in land_count.values()])
+                for i in range(50):
+                    if len(crawledItems) == totalLandCount:
+                        break
+                    crawledItems = driver.find_elements(By.CLASS_NAME, "item_inner")
+                    # if maxLen == len(crawledItems) and totalLandCount > maxLen:
+                    #     time.sleep(time_value)
+                    #     time_value = time_value + 1
+                    # else:
+                    #     time_value = 5
+                    click_idx = -1
+                    try:
+                        time.sleep(self.time_sleep_int2)
+                        if "네이버에서 보기" in crawledItems[click_idx].text:
+                            view_naver = crawledItems[click_idx].find_element(By.LINK_TEXT, '네이버에서 보기')
+                            time.sleep(self.time_sleep_int1)
+                            view_naver.click()
+                        else:
+                            time.sleep(self.time_sleep_int1)
+                            crawledItems[click_idx].click()
+                    except Exception as ex:
+                        self.logger.critical(f"click error")
+
+                    html = driver.page_source
+                    # soup에 넣어주기
+                    soup = bs(html, 'html.parser') #.encode("utf-8")
+                    self.logger.info(
+                        f"{i + 1}: 수집({len(crawledItems)}) | 대상({totalLandCount}) | {round((len(crawledItems) / totalLandCount) * 100)}% )")
+                    if len(crawledItems) <= maxLen and len(crawledItems) >= totalLandCount:
+                        break
+                    else:
+                        maxLen = len(crawledItems)
+            landItems = soup.findAll('div', 'item_inner')
+            self.logger.info(f"{complex_title} 매물 수집({len(crawledItems)}건) 완료")
+            res = []
+            except_cnt = 0
+            for index, value in enumerate(landItems):
+                try:
+                    if value.find('em', 'data').string < self.confirm_limit_day:
+                        except_cnt = except_cnt + 1
+                        continue
+                    tmp_dict = {}
+                    tmp_dict["confirm_date"] = value.find('em', 'data').string  # 23.07.13.
+                    tmp_dict["complex_title"] = landName #complex_title
+                    # tmp_dict["land_title"], tmp_dict["dong"] = (value.find('div','item_title').find('span','text').string).split(' ') #샛별삼부 408동
+                    tmp_dict["item_title"] = value.find('div', 'item_title').find('span', 'text').string  # 샛별삼부 408동
+                    tmp_dict["price_type"] = value.find('div', 'price_line').find('span', 'type').string #전세
+                    if tmp_dict["price_type"] in ['매매','전세']: #매매, 전세
+                        tmp_dict["price"] = value.find('div', 'price_line').find('span', 'price').string #7억 5,000
+                        tmp_dict["price_int"] = self.get_price_int(value.find('div', 'price_line').find('span', 'price').string, tmp_dict["price_type"])
+                    else: #월세
+                        tmp_dict["price"] = value.find('div', 'price_line').get_text()[2:]
+                        tmp_dict["price_int"] = self.get_price_int(tmp_dict["price"].split('/')[0])
+                    tmp_dict["land_type"] = value.find('div', 'info_area').find('strong','type').string #아파트
+                    # tmp_dict["info_area_spec"] = (value.find('div', 'info_area').find('span','spec').string).replace('\n','') #158/128m², 15/25층, 남동향
+                    tmp_dict["area_m"], tmp_dict["floor"], tmp_dict["direction"] = (value.find('div', 'info_area').find('span', 'spec').string).replace('\n', '').split(',')  # 158/128m², 15/25층, 남동향
+                    tmp_dict["area_p"] = self.get_area_num(tmp_dict["area_m"])
+                    tmp_dict["info_area_spec2"] = (value.find('div', 'info_area').get_text().split(',')[-1]).replace('\n','').strip() #8월중입주협의
+                    #tmp_dict["raw_text"] = value.get_text().replace('\n','')
+                    res.append(tmp_dict)
+                except Exception as ex:
+                    self.logger.critical(f"{complex_title} 파싱 실패: {value}")
+                    self.logger.critical(f"{ex}")
+            self.logger.info(f"{complex_title}: {len(res)}건 (*{self.confirm_limit_day}이전: {except_cnt}건 )")
+            return res
+        except Exception as ex:
+            self.logger.critical(f"get_land_info Error")
+            self.logger.critical(f"{ex}")
+            return -2
 
     def get_area_num(self, area_m):
         try:
@@ -334,6 +363,7 @@ class landCrawling():
 
     def main(self):
         self.logger.info("="*50)
+        self.print_config_info()
         urls = self.read_url_file()
         ladnList = self.start_crawling(urls) #매개변수: url, return 값: list[dict{}]
         df, brif_df = self.list_to_df(ladnList)
